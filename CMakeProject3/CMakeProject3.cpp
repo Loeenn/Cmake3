b76c23a9b1aeacd562d07151dfcf6c88246d8c33
senya <senya@SEMYONANUFR7142>
2022-11-26 21:24:23 +0300

    Добавьте файлы проекта.

---
+++ CMakeProject3/CMakeProject3.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject3.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
